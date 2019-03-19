130bf6787df55d0c7ae323c39eeb9ae2c304423a
Siddharth Shrotriya <sidshro09@gmail.com>
2019-03-20 00:24:05 +0530

    Node Assignment 1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// code for simple http server
+
 /*const express = require('express');
 const http = require('http');
 
@@ -53,6 +55,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -60,7 +64,13 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
+
+//app.use('/dishes/:dishId', dishRouter);
+
+/*Code for REST API without router*/
 /* app.all('/dishes', (req,res,next) => {
 	res.ststusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
